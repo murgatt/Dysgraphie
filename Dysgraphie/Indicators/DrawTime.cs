a2d5f1dd030fe4fe91e8a2083c1ffb651be01492
BCedric <skalimero@gmx.fr>
2016-05-10 16:06:36 +0200

    temps de tracé

---
+++ Dysgraphie/Indicators/DrawTime.cs
@@ -0,0 +1,21 @@
+﻿using Dysgraphie.Datas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dysgraphie.Indicators
+{
+    class DrawTime : AbstractIndicator
+    {
+
+        public DrawTime(List<Point> points, Analysis analysis) : base(points, analysis) { }
+
+
+        public override void calcul()
+        {
+            this.analysis.drawTime = this.points.ElementAt(this.points.Count - 1).t - this.points.ElementAt(0).t;
+        }
+    }
+}
