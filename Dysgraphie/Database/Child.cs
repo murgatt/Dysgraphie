3e9ca834025373f72466d0170d8668cf4da59626
BCedric <skalimero@gmx.fr>
2016-05-31 12:51:12 +0200

    up

--- Dysgraphie/Database/Child.cs
+++ Dysgraphie/Database/Child.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Dysgraphie.Database;
+using System.Data.SQLite;
 
 /* DB IS LIKE 
  * ID / Nom / Prenom / Age / Classe / Genre / Lateralite
@@ -14,7 +15,7 @@ namespace Dysgraphie.Database
 {
     public class Child
     {
-        private string ID;
+        private int ID;
         private string Nom;
         private string Prenom;
         private DateTime DateN;
@@ -22,22 +23,10 @@ namespace Dysgraphie.Database
         private string Classe;
         private string Genre;
         private string Lateralite;
-        private DbManager dBmanager;
 
-        public Child(string ID, string Nom, string Prenom, int Age, string Classe, string Genre, string Lateralite)
-        {
-            this.ID = ID;
-            this.Nom = Nom;
-            this.Prenom = Prenom;
-            this.Age = Age;
-            this.Classe = Classe;
-            this.Genre = Genre;
-            this.Lateralite = Lateralite;
-            this.dBmanager = new DbManager();
-            this.dBmanager.DBConnexion("myDB");
-        }
 
-        public Child(String name, String forename, DateTime birth, String grade, String laterality, String gender)
+
+        public Child(int ID, String name, String forename, DateTime birth, String grade, String laterality, String gender)
         {
             this.Nom = name;
             this.Prenom = forename;
@@ -53,6 +42,7 @@ namespace Dysgraphie.Database
             }
         }
 
+
         public void EditChild( string Nom, string Prenom, int Age, string Classe, string Genre, string Lateralite)
         {
             this.Nom = Nom;
@@ -63,7 +53,7 @@ namespace Dysgraphie.Database
             this.Lateralite = Lateralite;
         }
 
-        public string GetID()
+        public int GetID()
         {
             return this.ID;
         }
@@ -75,10 +65,12 @@ namespace Dysgraphie.Database
         {
             return this.Prenom;
         }
+
         public DateTime GetDateN()
         {
             return this.DateN;
         }
+
         public int GetAge()
         {
             return this.Age;
@@ -96,10 +88,30 @@ namespace Dysgraphie.Database
             return this.Lateralite;
         }
 
-        public void AddChildInDB()
+
+        public bool alreadySaved(DbManager dBmanager)
         {
-            string req = "Insert into ListChildren values ('" + this.ID + "','" + this.Nom + "','" + this.Prenom + "','" + this.Age + "','" + this.Classe + "','" + this.Genre + "','" + this.Lateralite + "' );";
+            dBmanager.DBConnexion();
+            string req = "select * from Children WHERE Nom ='" + this.Nom + "' AND Prenom='" + this.Prenom + "' AND Age='" + this.Age + "' AND Classe='" + this.Classe + "' AND Genre='" + this.Genre + "' AND Lateralite='" + this.Lateralite + "'";
+            SQLiteDataReader reader = dBmanager.QueryRequest(req);
+
+            while (reader.Read())
+            {
+                this.ID = Convert.ToInt32(reader.GetInt32(0));
+                dBmanager.DBDeconnexion();
+                return true;
+            }
+            dBmanager.DBDeconnexion();
+            return false;
+        }
+
+
+        public void AddChildInDB(DbManager dBmanager)
+        {
+            dBmanager.DBConnexion();
+            string req = "Insert into Children values ('" + this.ID + "','" + this.Nom + "','" + this.Prenom + "','" + this.Age + "','" + this.Classe + "','" + this.Genre + "','" + this.Lateralite + "' );";
             dBmanager.QueryRequest(req);
+            dBmanager.DBDeconnexion();
         }
     }
 }
